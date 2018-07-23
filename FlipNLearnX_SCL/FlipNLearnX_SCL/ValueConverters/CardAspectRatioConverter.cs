90afebadb229dbb3d502435dc8de32efc1649634
Corbin Trexler <ctrexler@outlook.com>
2018-07-23 14:46:39 -0400

    Cards now maintain aspect ratio on resizing; Fixed other card display issues

---
+++ FlipNLearnX_SCL/FlipNLearnX_SCL/ValueConverters/CardAspectRatioConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FlipNLearnX_SCL.ValueConverters
+{
+    class CardAspectRatioConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (System.Convert.ToInt32(value)*3)/5;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
